4372698c3130aa2c4ab2c2433f1baa1762f0e5b2
John Martinez <jmartinez729@gmail.com>
2018-11-13 17:00:04 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,21 @@
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  
+  cart.each do |grocery_hash|
+    grocery_hash.each do |groceries, prices|
+      if consolidated_cart[groceries].nil?
+        consolidated_cart[groceries] = prices.merge({:count => 1})
+      else 
+        consolidated_cart[groceries][:count] += 1
+      end 
+    end 
+  end 
+  
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
